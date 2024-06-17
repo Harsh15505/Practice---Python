0b770bc7c0d5fa07945b9db2585868754c9fc28b
Harsh Bhavsar <bhavsarharsh155@gmail.com>
2024-06-17 18:58:51 +0530

    ERROR Handling

--- API handling/Air Pollution.py
+++ API handling/Air Pollution.py
@@ -5,47 +5,64 @@ api_key = Credentials.api_key
 
 def geocoder(user_input):
     
-    data = requests.get(f"http://api.openweathermap.org/geo/1.0/direct?q={user_input}&appid={api_key}")
-    location_data = data.json()
-    coordinates = [location_data[0]['lat'], location_data[0]['lon']]
-    return coordinates
+    try:
+        data = requests.get(f"http://api.openweathermap.org/geo/1.0/direct?q={user_input}&appid={api_key}")
+        data.raise_for_status()
+        location_data = data.json()
+        if not location_data:
+            print("Location not found. Please try again.")
+            return None
+        coordinates = [location_data[0]['lat'], location_data[0]['lon']]
+        return coordinates
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching the location data : {e}")
+        return None
 
 
 def fetch_air_pollution_data(coordinates):
-
-    data = requests.get(f"https://api.openweathermap.org/data/2.5/air_pollution?lat={coordinates[0]}&lon={coordinates[1]}&appid={api_key}")
-    air_pollution_data = data.json()
-    return air_pollution_data
+    try:
+        data = requests.get(f"https://api.openweathermap.org/data/2.5/air_pollution?lat={coordinates[0]}&lon={coordinates[1]}&appid={api_key}")
+        data.raise_for_status()
+        air_pollution_data = data.json()
+        return air_pollution_data
+    
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching the air pollution data : {e}")
+        return None
 
 def main():
     print("------------------WELCOME TO AIR POLLUTION REPORT------------------")
     location = input("Enter your location: ")
     print('---------------------------------------------------------------')
     coordinates = geocoder(location)
-    air_pollution_data = fetch_air_pollution_data(coordinates)
-    
-    aqi = air_pollution_data['list'][0]['main']['aqi']
-    print(f"*** AIR POLLUTION REPORT OF {location} ***")
-    match aqi:
-        case 1:
-            print("AQI: Good. Air quality is satisfactory, and air pollution poses little or no risk.")
-        
-        case 2:
-            print("AQI: Fair. Air quality is acceptable. However, there may be a risk of health effects.")
-        
-        case 3:
-            print("AQI: Moderate. There may be a risk of health effects for some people, particularly those who are unusually sensitive to air pollution.")
-        
-        case 4:
-            print("AQI: Unhealthy for sensitive groups. People with lung disease, older adults, children, and individuals with heart or lung disease can be at greater risk from exposure to air pollution.")
-        
-        case 5:
-            print("AQI: Unhealthy. Everyone may begin to experience health effects, especially children and individuals with underlying health conditions.")
-    
-    print("-"*70)
-    print("\n\n**NOTE** \n AQI is abbreviation for 'Air Quality Index'")
+    if coordinates:
+        air_pollution_data = fetch_air_pollution_data(coordinates)
+       
+        if air_pollution_data:    
+            
+            aqi = air_pollution_data['list'][0]['main']['aqi']
+            print(f"*** AIR POLLUTION REPORT OF {location} ***")
+            match aqi:
+                case 1:
+                    print("AQI: Good. Air quality is satisfactory, and air pollution poses little or no risk.")
+                
+                case 2:
+                    print("AQI: Fair. Air quality is acceptable. However, there may be a risk of health effects.")
+                
+                case 3:
+                    print("AQI: Moderate. There may be a risk of health effects for some people, particularly those who are unusually sensitive to air pollution.")
+                
+                case 4:
+                    print("AQI: Unhealthy for sensitive groups. People with lung disease, older adults, children, and individuals with heart or lung disease can be at greater risk from exposure to air pollution.")
+                
+                case 5:
+                    print("AQI: Unhealthy. Everyone may begin to experience health effects, especially children and individuals with underlying health conditions.")
+            
+            print("-"*70)
+            print("\n\n**NOTE** \n AQI is abbreviation for 'Air Quality Index'")
 
-    print("-"*70)
+        print("-"*70)
 
 
 
--- API handling/Weather API.py
+++ API handling/Weather API.py
@@ -4,10 +4,20 @@ import Credentials
 api_key = Credentials.api_key
 
 def fetch_weather_data(user_input):
-    data = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={user_input}&units=metric&appid={api_key}")
+    
+    try:
 
-    weather_data = data.json()
-    return weather_data
+        data = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={user_input}&units=metric&appid={api_key}")  
+        data.raise_for_status() 
+        weather_data = data.json()
+        if not weather_data:
+            print("Cannot fetch data. Please try again.")
+            return None
+        return weather_data
+    
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching the weather data : {e}")
+        return None
 
 
 def main():
@@ -16,16 +26,18 @@ def main():
     user_input = input("Please Enter your city:")
     weather_data = fetch_weather_data(user_input)
     
-    print('---------------------------------------------------------------')
-    print(f"\n\n*** WEATHER REPORT OF {user_input} ***")
-    print("-"*70)
+    if weather_data:
+        
+        print('---------------------------------------------------------------')
+        print(f"\n\n*** WEATHER REPORT OF {user_input} ***")
+        print("-"*70)
 
-    print(f"Weather = {weather_data['weather'][0]['main']} with {weather_data['weather'][0]['description']}")
-    print(f"Temperatue = {weather_data['main']['temp']} °C and it Feels Like = {weather_data['main']['feels_like']} °C")
-    print(f"Max Temperature of the day = {weather_data['main']['temp_max']} °C")
-    print(f"Humidity = {weather_data['main']['humidity']} %")
-    print(f"Pressure = {weather_data['main']['pressure']} hPa")
-    print("-"*70)
+        print(f"Weather = {weather_data['weather'][0]['main']} with {weather_data['weather'][0]['description']}")
+        print(f"Temperatue = {weather_data['main']['temp']} °C and it Feels Like = {weather_data['main']['feels_like']} °C")
+        print(f"Max Temperature of the day = {weather_data['main']['temp_max']} °C")
+        print(f"Humidity = {weather_data['main']['humidity']} %")
+        print(f"Pressure = {weather_data['main']['pressure']} hPa")
+        print("-"*70)
   
 
 if __name__ == "__main__":
